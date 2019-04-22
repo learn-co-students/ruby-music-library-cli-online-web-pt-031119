768d0ead168a4cd3d2f9e3d0284a2cbebd9b3bff
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-22 22:54:11 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,8 +1,14 @@
 class Genre
   extend Concerns::Findable
+<<<<<<< HEAD
 
   attr_accessor :name, :songs
 
+=======
+  
+  attr_accessor :name
+  attr_reader :songs
+>>>>>>> 98c73fd34e15b7ad64ab0e67f259a4aa3bc54a93
   @@all = []
 
   def initialize(name)
@@ -29,7 +35,11 @@ class Genre
   end
 
   def artists
+<<<<<<< HEAD
     @songs.collect {|song| song.artist}.uniq
+=======
+    songs.collect {|song| song.artist}.uniq
+>>>>>>> 98c73fd34e15b7ad64ab0e67f259a4aa3bc54a93
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist, :genre
   @@all = []
@@ -39,6 +40,10 @@ class Song
 
   def self.find_by_name(name)
     all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || create(name)
   end
 
   def self.new_from_filename(file)
