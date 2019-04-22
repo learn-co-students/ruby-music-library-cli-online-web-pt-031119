1ebe3705cf980fda999cbc274d3252b6b30aa1ff
Mark Kanko <mkanko66@gmail.com>
2019-04-21 21:26:08 -0700

    added methods through 006_artists_and_genres

---
+++ lib/artist.rb
@@ -0,0 +1,63 @@
+require 'pry'
+
+class Artist
+    attr_accessor :artist, :song, :genre 
+    attr_reader :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+        #@genres = []   
+    end 
+
+    def name= (name)
+        @name = name 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def songs
+        @songs
+    end
+
+    def genres
+        self.songs.collect {|song| song.genre if song.genre}.uniq
+    end 
+
+    def save
+        self.class.all << self # same as - @@all << self - just more abstracted 
+    end 
+
+    def self.destroy_all
+        @@all.clear 
+    end 
+
+    def self.create(name)
+        artist = self.new(name)
+        artist.save 
+        artist  
+    end
+
+    def add_song(song)
+        if self.songs.length == 0 
+            self.songs << song
+            song.artist = self if !song.artist 
+        else  
+            self.songs.each do |s| 
+                if s == song 
+                    self.song    
+                else  
+                    self.songs << song
+                    song.artist = self if !song.artist 
+                end 
+            end
+        end       
+    end 
+    
+     
+
+end 
---
+++ lib/song.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+class Song 
+
+    attr_accessor :song, :artist, :genre 
+    attr_reader :name
+
+    @@all = []
+
+     
+    def initialize(name, artist=nil, genre=nil)  
+        @name = name
+        self.artist= (artist) if artist
+        self.genre= (genre) if genre     
+    end
+    
+    def name= (name)
+        @name = name 
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def save 
+        self.class.all << self  
+    end 
+
+    def self.destroy_all
+        @@all.clear
+    end 
+
+    def self.create(name)
+        song = self.new(name)  
+        song.save 
+        song  
+    end 
+
+    def artist= (artist)
+        @artist = artist
+        artist.add_song(self)  
+    end 
+    
+    def genre= (genre)
+        @genre = genre 
+        genre.add_song(self) 
+    end 
+
+    def self.find_by_name(name)
+        self.all.detect {|song| song.name == name}
+    end
+    
+    def self.find_or_create_by_name(name)
+        self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+    end 
+        
+
+end 
+     
