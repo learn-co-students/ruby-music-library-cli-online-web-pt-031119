799a8fb34186a7a6cc8176abebb9b18dc9093327
David Cantoran <davidcantoran36@gmail.com>
2019-04-23 04:04:19 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist 
+  extend Concerns::Findable
+  
+  attr_accessor :name 
+  attr_reader :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def genres 
+    songs.map(&:genre).uniq
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
+  
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end 
+  
+  def add_song(song)
+    song.artist = self unless song.artist
+    @songs << song unless @songs.include?(song)
+  end 
+end 
