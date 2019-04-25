d40601799208bc4fd5b800424668c9eb0639fb45
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-25 01:04:04 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist 
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(artist)
+    created_artist = Artist.new(artist)
+    created_artist.save
+    created_artist
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all
+    @@all.clear 
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(song) 
+    created_song = Song.new(song)
+    created_song.save
+    created_song
+  end
+  
+end 
