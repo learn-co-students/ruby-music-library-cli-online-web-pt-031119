9ddfade2432e9479e725a2ed245091d763328515
Leeya Davis <leeya463@gmail.com>
2019-04-26 00:17:40 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,53 +8,58 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+    input = nil
+    until input == "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
 
-    4.times do
-      input = gets.chomp! unless input == "exit"
+      input = gets.chomp
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
     end
 
   end
 
-  def class_sort(klass)
-    klass.all.sort_by{|obj| obj.name}
-  end
-
   def list_songs
-    class_sort(Song).each.with_index(1) do |song, i|
+    Song.all.sort_by(&:name).each.with_index(1) do |song, i|
      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 
    def list_artists
-     class_sort(Artist).each.with_index(1) do |singer, i|
+     Artist.all.sort_by(&:name).each.with_index(1) do |singer, i|
       puts "#{i}. #{singer.name}"
       end
     end
 
     def list_genres
-      class_sort(Genre).each.with_index(1) do |genre, i|
-        puts "#{i}. #{genre.name}"
-      end
+      Genre.all.sort_by(&:name).each.with_index(1) do |genre, i|
+       puts "#{i}. #{genre.name}"
+       end
     end
-
-    def get_artist(input)
-      Artist.all.find {|singer| singer.name == input}
-      #take in users input & find said artist in Artist class
-    end
-
 
     def list_songs_by_artist
       puts "Please enter the name of an artist:"
-      input = gets.strip #<need to know the artist
+      input = gets.strip
 
       if artist = Artist.find_by_name(input)
         artist.songs.sort_by(&:name).each.with_index(1) do |song, i|
@@ -75,18 +80,11 @@ class MusicLibraryController
     end
 
     def play_song
-      list_songs
       puts "Which song number would you like to play?"
       input = gets.strip.to_i
-      #binding.pry
-      #if input == >= 1 && input == <= list_songs.size #ensures input is valid
-        #w/ valid input, selects song accordingly...we need to iterate over Song.name to find the song
-        
-#accepts user input which will point to a number
-#upon receiving valid input 'plays' the matching song from the alphabetized list output by #list_songs
-#to receive(:puts).with("Which song number would you like to play?")
-#to receive(:puts).with("Playing Larry Csonka by Action Bronson")
-#doesn't puts anything out if a matching song isn't found
-#checks that user entered a number between 1 and the total number of songs in library otherwise it does nothing
+      if (1..Song.all.length).include?(input)
+        song = Song.all.sort_by(&:name)[input-1]
+        puts "Playing #{song.name} by #{song.artist.name}" if song
+      end
     end
 end
